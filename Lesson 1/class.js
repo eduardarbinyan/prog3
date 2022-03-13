bfe2c46f5610238a531e90213a8e1541069ad9d0
xya8 <armen.karapetyan02@tumo.org>
2022-03-13 10:57:06 +0400

    suc

--- Lesson 1/class.js
+++ Lesson 1/class.js
@@ -1,5 +1,5 @@
-class Grass {
-    constructor(x, y) {
+class Creature{
+	constructor(x, y) {
         this.x = x;
         this.y = y;
         this.multiply = 0
@@ -14,7 +14,7 @@ class Grass {
             [this.x + 1, this.y + 1]
         ];
     }
-    chooseCells(character) {
+    chooseCell(character) {
         var found = [];
         for (var i in this.directions) {
             var x = this.directions[i][0];
@@ -25,28 +25,6 @@ class Grass {
             }
         }
         return found;
-    }
-    mul() {
-        let emptyCells = this.chooseCells(0)
-        this.multiply++
-        if (emptyCells.length != 0 && this.multiply >= 5) {
-            let randomCell = random(emptyCells)
-            let x = randomCell[0]
-            let y = randomCell[1]
-            matrix[y][x] = 1
-            let gr = new Grass(x, y)
-            grassArr.push(gr)
-            this.multiply = 0
-        }
-    }
-}
-class GrassEater {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.energy = 7;
-        this.multiply = 0
-        this.directions = [];
     }
     getNewCoordinates() {
         this.directions = [
@@ -60,21 +38,26 @@ class GrassEater {
             [this.x + 1, this.y + 1]
         ];
     }
-
-    chooseCell(character) {
-        this.getNewCoordinates()
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i]);
-                }
-            }
+}
+class Grass extends Creature {
+    mul() {
+        let emptyCells = this.chooseCell(0)
+        this.multiply++
+        if (emptyCells.length != 0 && this.multiply >= 5) {
+            let randomCell = random(emptyCells)
+            let x = randomCell[0]
+            let y = randomCell[1]
+            matrix[y][x] = 1
+            let gr = new Grass(x, y)
+            grassArr.push(gr)
+            this.multiply = 0
         }
-        return found;
+    }
+}
+class GrassEater extends Creature{
+    constructor(x, y) {
+        super(x,y)
+        this.energy=8;
     }
     mul() {
         var emptyCells = this.chooseCell(0);
@@ -148,25 +131,11 @@ class GrassEater {
         }
     }
 }
-class Predator {
+class Predator extends Creature {
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.energy = 12
-        this.multiply = 0
-        this.directions = []
-    }
-    getNewCoordinates() {
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
+    	super(x,y)
+    	this.energy=8;
+
     }
     chooseCell(character) {
         this.getNewCoordinates()
@@ -257,39 +226,9 @@ class Predator {
         }
     }
 }
-class Ghost {
-    constructor(x, y) {
-        this.x = x
-        this.y = y
-        this.directions = []
-    }
-    getNewCoordinates() {
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
-    }
-    chooseCells(character) {
-        this.getNewCoordinates()
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && y >= 0 && x < matrix.length && y < matrix.length) {
-                if (matrix[y][x] == character)
-                    found.push(this.directions[i]);
-            }
-        }
-        return found;
-    }
+class Ghost extends Creature {
     consume() {
-        var emptyCells = this.chooseCells(0)
+        var emptyCells = this.chooseCell(0)
         var newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
         if (newCell) {
             var newX = newCell[0]
@@ -320,7 +259,7 @@ class Void {
 class Bomber {
     constructor(x, y) {
         this.x = x
-        this, y = y
+        this.y = y
     }
     fire() {
         var random1 = Math.floor(random(1, msize - 1))
