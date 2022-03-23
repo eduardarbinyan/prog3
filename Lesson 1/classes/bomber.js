49a7b79670c9fbde5b3cc644b80e558ba1f90663
eduardarbinyan <101492786+eduardarbinyan@users.noreply.github.com>
2022-03-23 18:28:58 +0400

    Add files via upload

---
+++ Lesson 1/classes/bomber.js
@@ -0,0 +1,19 @@
+module.exports = class Bomber{
+    constructor(x, y) {
+        this.x = x
+        this, y = y
+    }
+    // fire() {
+    //     var random1 = Math.floor(Math.random()*msize)
+    //     var random2 = Math.floor(Math.random()*msize)
+    //     matrix[random1][random2] = 0
+    //     matrix[random1 + 1][random2 + 1] = 0
+    //     matrix[random1 - 1][random2 - 1] = 0
+    //     matrix[random1 - 1][random2] = 0
+    //     matrix[random1][random2 - 1] = 0
+    //     matrix[random1 + 1][random2] = 0
+    //     matrix[random1][random2 + 1] = 0
+    //     matrix[random1 - 1][random2 + 1] = 0
+    //     matrix[random1 + 1][random2 - 1] = 0
+    // }
+}
